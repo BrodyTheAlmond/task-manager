ba5e64f32d70532c8cb727b6ee407a2169e879ca
JoMama <jordyalmonte@icloud.com>
2026-08-13 10:49:20 -0400

    refactor/ had to change folder structure as package files were stored inside src directory. chnaged folder structure to src->task_mangager->package files

---
+++ src/task_manager/cli.py
@@ -0,0 +1,34 @@
+import argparse
+from tasks import add, complete, delete, show, ensure_file
+
+
+def main(argv=None):
+    ensure_file()
+    parser = argparse.ArgumentParser(prog='task-manager')
+    subparsers = parser.add_subparsers(dest='command')
+
+    add_parser = subparsers.add_parser('add')
+    add_parser.add_argument('task')
+
+    complete_parser = subparsers.add_parser('complete')
+    complete_parser.add_argument('index', type=int)
+
+    delete_parser = subparsers.add_parser('delete')
+    delete_parser.add_argument('index', type=int)
+
+    show_parser = subparsers.add_parser('show')
+
+    args = parser.parse_args(argv)
+
+    if args.command == 'add':
+        add(args.task)
+    elif args.command == 'complete':
+        complete(args.index)
+    elif args.command == 'delete':
+        delete(args.index)
+    elif args.command == 'show':
+        show()
+
+
+if __name__ == '__main__':
+    main()
